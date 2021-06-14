e6442a02840922148b91bb850a14962f12d3f197
Garrick Suemith <garrick.suemith@gmail.com>
2021-06-14 11:19:28 -0400

    creates databasefile with authors and posts

--- data/migrations/20210614150601_blog-authors-posts.js
+++ data/migrations/20210614150601_blog-authors-posts.js
@@ -1,8 +1,28 @@
 
 exports.up = function(knex) {
-  
+  return knex.schema
+    .createTable('authors', tbl => {
+      tbl.increments();
+      tbl.string('name', 256)
+        .unique()
+        .notNullable();
+    })
+    .createTable('posts', tbl => {
+      tbl.increments();
+      tbl.string('title', 256).unique().notNullable();
+      tbl.text('body', 1024*16).notNullable();
+      tbl.integer('author_id')
+        .unsigned()
+        .notNullable()
+        .references('id')
+        .inTable('authors')
+        .onUpdate('CASCADE')
+        .onDelete('RESTRICT');
+    })
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema
+    .dropTableIfExists('posts')
+    .dropTableIfExists('authors');
 };
