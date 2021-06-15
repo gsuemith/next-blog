da45bc799d334c47e65268378154ebb088816b4f
Garrick Suemith <garrick.suemith@gmail.com>
2021-06-15 10:45:04 -0400

    refactors ArticleList

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,39 @@
 
+import Link from 'next/Link'
+import ArticleList from '../components/ArticleList'
 import Footer from '../components/Footer'
 import Meta from '../components/Meta'
+import {server} from '../config'
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
+export default function Home({ posts }) {
+  
   return (
+    <>
+    <Meta />
     <div className={styles.container}>
-      <Meta />
       <main className={styles.main}>
+        <div className={styles.title}>
+          <h4><a>NEXT.js</a> Blog</h4>
+        </div>
 
+        <ArticleList posts={posts} />
        
       </main>
 
       <Footer/>
     </div>
+    </>
   )
+}
+
+export const getStaticProps = async () => {
+  const res = await fetch(`${server}/api/posts`)
+  const posts = await res.json()
+
+  return {
+    props: {
+      posts
+    }
+  }
 }
